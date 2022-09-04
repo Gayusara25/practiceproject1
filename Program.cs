c3006c9029fbd2a10a40a0a6b8c05981b684b923
Gayusara25 <111350059+Gayusara25@users.noreply.github.com>
2022-09-04 23:06:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace practiceproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("C:\\Users\\Gayathri\\Desktop\\practice project\\studentdata.txt");
+            Console.WriteLine(data);
+            Console.ReadLine();
+        }
+    }
+}
